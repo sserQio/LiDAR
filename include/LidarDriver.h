cca8d14966205edb7cfd9a48fc670d83381bbab8
sserQio <papolino10@gmail.com>
2024-11-22 12:17:44 +0100

    header completato

--- include/LidarDriver.h
+++ include/LidarDriver.h
@@ -14,8 +14,34 @@ class LidarDriver {
         const int BUFFER_DIM; // Numero di scansioni
         const int angolo; // Angolo totale da spaziare, questo valore è fisso a 180 gradi
         const double ris_ang; // Angolo che separa le letture consecutive
-        std::vector<std::vector<double>> letture;
+        int counter; // Contatore per tenere traccia dello scan più nuovo
+        int size; // Contatore per vedere se il buffer è pieno
+        std::vector<std::vector<double>> total_scans;
 
+    public:
+        LidarDriver(int bufferSize, double angRis); // Costruttore con parametri BUFFER_DIM e ris_ang
+
+        int get_BUFFER_DIM(); // Metodo per ottenere BUFFER_DIM
+        double get_ris_ang(); // Metodo per ottenere ris_ang
+        
+        // Organizzando le scan in matrice usiamo coordinate x e y per navigare. La coordinata x indica una colonna.
+        // Una colonna comprende tutte le letture. Una colonna è quindi una scan. Fornendo il parametro y possiamo quindi
+        // scegliere quale lettura stampare 
+        void print_lettura(int x, int y); // Metodo per stampare una singola lettura
+        void print_scan(int x); // Metodo per stampare uno scan
+        void print_all_scans(); // Metodo per stampare la matrice con tutti gli scan
+        void remove_scan(int x); // Rimuove una scan (colonna) a scelta all'indice x
+        
+        void new_scan(std::vector<double>& scan); // Accetta una nuova scansione e lo memorizza nel buffer
+                                                  // e sovrascrive la meno recente se il buffer è pieno
+        std::vector<double>& get_scan(); // Fornisce uno scan contenente la più vecchia e la rimuove dal buffer
+        void clear_buffer(); // Elimina tutte le scansioni salvate
+        double get_distance(double angolo); // Accetta un angolo e ritorna la lettura corrispondente a tale angolo
+                                            // nella scansione più recente. Se non esiste tale angolo ritorna
+                                            // il valore di angolo più vicino
 };
+
+// Overload operatore
+std::ostream &operator<<(std::ostream& os, const std::vector<double>& scan);
 
 #endif // "LIDARDRIVER_H"
---
+++ src/LidarDriver.cpp
@@ -0,0 +1,7 @@
+// Progetto di Barison, Castellini e Petenazzo
+
+#include <iostream>
+#include "../include/LidarDriver.h"
+
+LidarDriver::LidarDriver(int BufferSize, double angRis) : BUFFER_DIM{BufferSize}, ris_ang{angRis}, angolo{180} {};
+
